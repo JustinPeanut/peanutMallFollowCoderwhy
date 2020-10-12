f5191e1f335401294adc5a8ae19268e7cf644563
JustinPeanut <1820840828@qq.com>
2020-10-12 16:42:11 +0800

    detail shoping done

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,33 +2,38 @@
 <div>
   <detail-nav-bar/>
   <detail-swiper :detailImage="detailImage"/>
+  <detail-base-info :goods="goods"/>
 </div>
 </template>
 
 <script>
   import DetailNavBar from "./childcomps/DetailNavBar";
   import DetailSwiper from "./childcomps/DetailSwiper";
+  import DetailBaseInfo from "./childcomps/DetailBaseInfo";
 
-  import {getDetail} from "network/detail/getDetail";
+  import {getDetail,Goods} from "network/detail/getDetail";
 
   export default {
     name: "Detail",
     components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo
     },
     data(){
       return {
         detailId: "",
-        detailImage: []
+        detailImage: [],
+        goods: {}
       }
     },
     created() {
       this.detailId = this.$route.params.iid
-      console.log(this.detailId)
       getDetail(this.detailId).then(res => {
+        const data = res.result
         // 保存轮播图信息
-        this.detailImage = res.result.itemInfo.topImages
+        this.detailImage = data.itemInfo.topImages
+        this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
       })
     }
   }
